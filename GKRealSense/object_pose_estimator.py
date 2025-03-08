f9457f8d6f0ee8cf8b89476019912d397cb66ce8
Teo Cardoso <teocardoso1@gmail.com>
2025-03-08 19:06:43 +0000

    Fix object pose estimator to use the correct points

--- GKRealSense/object_pose_estimator.py
+++ GKRealSense/object_pose_estimator.py
@@ -4,7 +4,7 @@ from object_detector import DetectedObject, ObjectType
 import numpy as np
 
 
-@dataclass(slot=True)
+@dataclass(slots=True)
 class ObjectWithPose:
     """Dataclass to store object with pose"""
 
@@ -27,17 +27,17 @@ class ObjectPoseEstimator:
         self, depth_frame: rs.depth_frame, detected_object: DetectedObject
     ) -> ObjectWithPose:
         # Improvement point: get the average from the pixels in the neighbourhood of the center point
-        z_distance: float = depth_frame.get_distance(
-            detected_object.box[0], detected_object.box[1]
-        )
-        x_distance, y_distance = rs.rs2_deproject_pixel_to_point(
+        x_point = int((detected_object.box[0] + detected_object.box[2]) / 2)
+        y_point = int((detected_object.box[1] + detected_object.box[3]) / 2)
+        z_distance: float = depth_frame.get_distance(x_point, y_point)
+        x_distance, y_distance, _ = rs.rs2_deproject_pixel_to_point(
             self.depth_intrinsics,
-            [detected_object.box[0], detected_object.box[1]],
+            [x_point, y_point],
             z_distance,
         )
+
         return ObjectWithPose(
-            detected_object.type,
-            np.array([x_distance, y_distance, z_distance], shape=(1, 3)),
+            detected_object.type, np.array([[x_distance, y_distance, z_distance]])
         )
 
     def estimate_pose(
