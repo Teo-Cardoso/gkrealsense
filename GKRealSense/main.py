7eedbfcfdf6462b4e82fd383747d97fc6d5c4dd7
Teo Cardoso <teocardoso1@gmail.com>
2025-03-08 19:14:20 +0000

    Add object pose estimator in main

--- GKRealSense/main.py
+++ GKRealSense/main.py
@@ -1,5 +1,7 @@
 from realsense_handler import RealSenseHandler, RealSenseConfig, FramesMix
 from object_detector import ObjectDetector, DetectedObject
+from object_pose_estimator import ObjectPoseEstimator, ObjectWithPose
+
 import cv2
 import numpy as np
 import time
@@ -8,6 +10,12 @@ import time
 def main():
     realsense = RealSenseHandler(RealSenseConfig())
     obj_detector = ObjectDetector(color_model_name="", ir_model_name="")
+    obj_pose_estimator = ObjectPoseEstimator(
+        realsense.depth_intrinsics,
+        color_intrinsics=realsense.color_intrinsics,
+        ir_intrinsics=realsense.ir_intrinsics,
+    )
+
     frame_count: int = 0
     start_time = time.perf_counter()
     while True:
@@ -17,15 +25,36 @@ def main():
             second_image = cv2.cvtColor(second_image, cv2.COLOR_GRAY2BGR)
 
         result: list[DetectedObject] = obj_detector.detect(frames_mix, second_image)
+        result_pose: list[ObjectWithPose] = obj_pose_estimator.estimate_pose(
+            depth_frame, result
+        )
 
-        for obj in result:
-            x1, y1, x2, y2 = obj.box
-            cv2.rectangle(second_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            label = f"{str(obj.type)}:{obj.confidence:.2f}"
+        for obj_index in range(len(result)):
+            x1, y1, x2, y2 = result[obj_index].box
+            cv2.circle(
+                second_image,
+                (int((x1 + x2) / 2), int((y1 + y2) / 2)),
+                5,
+                (0, 255, 0),
+                -1,
+            )
+
+            label = f"{str(result[obj_index].type)}:{result[obj_index].confidence:.2f}"
             cv2.putText(
                 second_image,
                 label,
                 (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2,
+            )
+
+            x, y, z = list(result_pose[obj_index].pose)[0]
+            cv2.putText(
+                second_image,
+                f"{x:.2f}, {y:.2f}, {z:.2f}",
+                (x1, y2 + 10),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.5,
                 (0, 255, 0),
